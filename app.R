dea396d475ff962d1f08bc77ebbe19d1832d98ac
pazremersaro <pazremersaro@gmail.com>
2025-10-29 18:25:39 -0300

    esqueleto

--- app.R
+++ app.R
@@ -1,1 +1,36 @@
 #       Probando el primer grafico            #
+
+# CARGA Y MANEJO DE DATOS
+library(readr)
+
+datos <- read_delim("cookies.txt", delim = "\t",
+                    escape_double = FALSE, trim_ws = TRUE)
+
+
+
+
+# ARMAMOS LA INTERFAZ
+
+MiInterfaz <- bslib::page_fluid(
+  shiny::titlePanel("Google Analytics"),
+  bslib::layout_sidebar(
+    sidebar = bslib::sidebar(),
+  )
+)
+
+
+
+
+
+
+
+
+
+# ARMAMOS EL SERVIDOR
+
+MiServidor <- function(input, output) {
+  
+}
+
+
+shiny::shinyApp(ui = MiInterfaz, server = MiServidor)
